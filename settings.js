c0454f97b6e89332c823de688196d314556b4223
Patch <107245549+patch777@users.noreply.github.com>
2022-06-14 15:27:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'd8bcfb256e', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
